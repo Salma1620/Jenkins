15c4696e3b05814f37a8b99cb3dcbaa125891b38
Salma EL HASSNAOUI <127985387+Salma1620@users.noreply.github.com>
2024-12-04 10:53:04 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
-public class HelloWrold{
-  public void print() {
-    System.out.println("hello world to Salma");
+public class HelloWorld{
+   public static void main(String args[]){
+      System.out.println("hello world to Salma");
   }
 }
+  
